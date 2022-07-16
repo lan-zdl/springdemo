28662b311761e688b5d9d8ac7010784bc9c29d81
1871159858@qq.com <1871159858@qq.com>
2022-07-16 11:02:32 +0800

    add cookie

--- src/main/java/com/example/zhang/springdemo/controller/IndexController.java
+++ src/main/java/com/example/zhang/springdemo/controller/IndexController.java
@@ -1,16 +1,34 @@
 package com.example.zhang.springdemo.controller;
 
+import com.example.zhang.springdemo.mapper.UserMapper;
+import com.example.zhang.springdemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String index(){
-        return "index";
-    }
+    @Autowired
+    private UserMapper userMapper;
 
+    @GetMapping("/index")
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies)
+            if(cookie.getName().equals("token")) {//判断是否拿到token的cookie,通过token来获取user；
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);//在数据库中查找是否存在token这条记录
+                if (user != null) {//如果存在将user放入session中；
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        return "index";
+        }
 }
